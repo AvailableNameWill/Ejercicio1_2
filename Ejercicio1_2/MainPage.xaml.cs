9ad170852928ff442ac09db468324e1f10e1d97a
AvailableNameWill <carloswill06@gmail.com>
2023-05-23 15:31:50 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1_2/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Security;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio1_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(){
+            InitializeComponent();
+        }
+
+        async void btnSendInfo_Clicked(object sender, EventArgs e) {
+            if (!String.IsNullOrEmpty(txtName.Text) || !String.IsNullOrWhiteSpace(txtName.Text) ||
+                !String.IsNullOrEmpty(txtSecName.Text) || !String.IsNullOrWhiteSpace(txtSecName.Text) ||
+                !String.IsNullOrEmpty(txtAge.Text) || !String.IsNullOrWhiteSpace(txtAge.Text) ||
+                !String.IsNullOrEmpty(txtEmail.Text) || !String.IsNullOrWhiteSpace(txtEmail.Text))
+            {
+                string _name = txtName.Text;
+                string _secName = txtSecName.Text;
+                int _age = Convert.ToInt32(txtAge.Text);
+                string _email = txtEmail.Text;
+                var persona = new Persona {
+                    name = _name, sec_Name = _secName, age = _age, email = _email
+                };
+                var dataPage = new DataPage();
+                dataPage.BindingContext = persona;
+                await Navigation.PushAsync(dataPage);
+            }
+            else Console.WriteLine("vacio");
+        }
+
+        private void clean(){
+            
+        }
+    }
+}
---
+++ Ejercicio1_2/Persona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Ejercicio1_2
+{
+    public class Persona
+    {
+        public string name { set; get; }
+        public string sec_Name { set; get; }
+        public int age { set; get; }
+        public string email { set; get; }
+
+        //public Persona() { }
+
+        /*public Persona(string _name, string _sec_Name, int _age, string _email){
+            setName(_name);
+            setSecName(_sec_Name);
+            setAge(_age);
+            setEmail(_email);
+        }
+
+        public void setName(string _name){ name = _name; }
+
+        public void setSecName(string _secName){ sec_Name = _secName; }
+
+        public void setAge(int _age){ age = _age; }
+
+        public void setEmail(string _email){ email = _email; }
+
+        public string getName(){ return name; }
+
+        public string getSecName() { return sec_Name; }
+
+        public int getAge() { return age; }
+
+        public string getEmail() { return email; }*/
+    }
+}
